95e7a43d6a470b98df4f12ebac6b6b34aaf20b47
MAC <mac@KashParas-Macbook-Pro-13.local>
2024-11-02 19:34:53 +0100

    intial commit

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,10 @@ app_name= "app"
 
 urlpatterns = [
     path('', landing, name="landing_page"),
+    path('login/', login, name='login'),
+    path('contact/', contact, name='contact'),
+    path('single/', single_product, name="single_product"),
+    path('all/', all_product, name="all_product"),
 
     
     
--- app/views.py
+++ app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+
+def landing(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def single_product(request):
+    return render(request, 'single-product.html')
+
+def all_product(request):
+    return render( request, 'all-product.html')
